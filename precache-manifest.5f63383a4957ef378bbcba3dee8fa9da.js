f4236c25c220c123b4fa34ca98b48f7b9ba42202
omareismail <omarehismail@gmail.com>
2022-09-20 19:29:26 +0300

    Updates

---
+++ precache-manifest.5f63383a4957ef378bbcba3dee8fa9da.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b808c196f00a7038e58",
+    "url": "/ReactProjects/static/js/main.3b808c19.chunk.js"
+  },
+  {
+    "revision": "4012972e0aaef86393e6",
+    "url": "/ReactProjects/static/js/1.4012972e.chunk.js"
+  },
+  {
+    "revision": "478252e0c61a3cb9dae1",
+    "url": "/ReactProjects/static/js/runtime~main.478252e0.js"
+  },
+  {
+    "revision": "62323a0fb7abc670b77961e17f1cbd8e",
+    "url": "/ReactProjects/index.html"
+  }
+];
